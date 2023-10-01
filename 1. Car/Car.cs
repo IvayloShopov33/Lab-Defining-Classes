a75ef7360b9559eae94cf3a0c1751ccce4a8eb0f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-02 02:05:41 +0300

    Add files via upload

---
+++ 1. Car/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public bool CheckIfTheYearIsValid()
+        {
+            if (Year >= 1886)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 1. Car/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            string carsDetails = string.Empty;
+            while (true)
+            {
+                carsDetails = Console.ReadLine();
+                if (carsDetails == "End")
+                {
+                    foreach (Car item in cars)
+                    {
+                        Console.WriteLine($"Make: {item.Make}\nModel: {item.Model}\nYear: {item.Year}\n");
+                    }
+
+                    break;
+                }
+
+                string[] newDetails = carsDetails.Split(' ');
+                string make = newDetails[0];
+                string model = newDetails[1];
+                int yearOfProduction = int.Parse(newDetails[2]);
+
+                Car car = new Car();
+                car.Make = make;
+                car.Model = model;
+                car.Year = yearOfProduction;
+
+                if (car.CheckIfTheYearIsValid())
+                {
+                    cars.Add(car);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid year of production!");
+                }
+            }
+
+        }
+    }
+}
