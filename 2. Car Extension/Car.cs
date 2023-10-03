0001927f8b6a7d8c875aa58ab30f629451f74625
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-04 00:51:56 +0300

    Add files via upload

---
+++ 2. Car Extension/Car.cs
@@ -0,0 +1,42 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public bool CheckTheGivenNumericValues()
+        {
+            if (Year >= 1886 && FuelQuantity >= 0 && FuelConsumption >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public void Drive(double distance)
+        {
+            double result = this.FuelQuantity - (distance * this.FuelConsumption);
+            if (result >= 0)
+            {
+                this.FuelQuantity = result;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ 2. Car Extension/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            string input = string.Empty;
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    foreach (Car item in cars)
+                    {
+                        Console.WriteLine($"Make: {item.Make}\nModel: {item.Model}\nYear: {item.Year}\nFuel: {item.FuelQuantity:F2}\nFuel Consumption: {item.FuelConsumption:F2}");
+                    }
+
+                    break;
+                }
+
+                if (!input.StartsWith("Drive") && !input.StartsWith("Print last car's details"))
+                {
+                    string[] carDetails = input.Split(' ');
+                    Car car = new Car();
+                    string make = carDetails[0];
+                    string model = carDetails[1];
+                    int yearOfProduction = int.Parse(carDetails[2]);
+                    double fuelQuantity = double.Parse(carDetails[3]);
+                    double fuelConsumption = double.Parse(carDetails[4]);
+
+                    car.Make = make;
+                    car.Model = model;
+                    car.Year = yearOfProduction;
+                    car.FuelQuantity = fuelQuantity;
+                    car.FuelConsumption = fuelConsumption;
+
+                    if (car.CheckTheGivenNumericValues() && !cars.Contains(car))
+                    {
+                        cars.Add(car);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid action! Either this car has already been added to the list or the given parameters are invalid.");
+                    }
+                }
+                else
+                {
+                    Car lastAddedCar = cars.LastOrDefault();
+                    if (lastAddedCar != null)
+                    {
+                        if (input.StartsWith("Drive"))
+                        {
+                            string[] drivenCarDetails = input.Split(" ");
+                            double distanceToTravel = double.Parse(drivenCarDetails[1]);
+                            lastAddedCar.Drive(distanceToTravel);
+                        }
+                        else
+                        {
+                            Console.WriteLine(lastAddedCar.WhoAmI());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
