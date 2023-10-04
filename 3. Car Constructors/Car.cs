cf22f80f400030958dd13a93089599ed342d2f4c
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-04 23:04:17 +0300

    Add files via upload

---
+++ 3. Car Constructors/Car.cs
@@ -0,0 +1,55 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int yearOfProduction) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = yearOfProduction;
+        }
+
+        public Car(string make, string model, int yearOfProduction, double fuelQuantity, double fuelConsumption) : this(make, model, yearOfProduction)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double result = this.FuelQuantity - (distance * this.FuelConsumption);
+            if (result >= 0)
+            {
+                this.FuelQuantity = result;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ 3. Car Constructors/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int yearOfProduction = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            cars.Add(firstCar);
+            Car secondCar = new Car(make, model, yearOfProduction);
+            cars.Add(secondCar);
+            Car thirdCar = new Car(make, model, yearOfProduction, fuelQuantity, fuelConsumption);
+            cars.Add(thirdCar);
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}\nFuel: {car.FuelQuantity:F2}\nFuel Consumption: {car.FuelConsumption:F2}\n");
+            }
+        }
+    }
+}
