7e0a00aa94dd66ade50b08e0f5d1291b92aa6e7e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-10-05 00:49:36 +0300

    Add files via upload

---
+++ 4. Car Engine and Tires/Car.cs
@@ -0,0 +1,56 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car(string make, string model, int yearOfProduction)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = yearOfProduction;
+        }
+
+        public Car(string make, string model, int yearOfProduction, double fuelQuantity, double fuelConsumption) : this(make, model, yearOfProduction)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int yearOfProduction, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, yearOfProduction,fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+        public void Drive(double distance)
+        {
+            double result = this.FuelQuantity - (distance * this.FuelConsumption);
+            if (result >= 0)
+            {
+                this.FuelQuantity = result;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ 4. Car Engine and Tires/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int carsCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= carsCount; i++)
+            {
+                Console.WriteLine("Write the details of your car: Make, Model, Year of production, Fuel quantity and consumption, Engine- horsepower and cubic capacity, 4 tires- years of use and pressure");
+                string[] carDetails = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string make = carDetails[0];
+                string model = carDetails[1];
+                int yearOfProduction = int.Parse(carDetails[2]);
+                double fuelQuantity = double.Parse(carDetails[3]);
+                double fuelConsumption = double.Parse(carDetails[4]);
+                int horsePower = int.Parse(carDetails[5]);
+                double cubicCapacity = double.Parse(carDetails[6]);
+                Engine engine = new Engine(horsePower, cubicCapacity);
+                Tire[] tires = new Tire[4];
+                for (int j = 0; j < tires.Length; j++)
+                {
+                    int year = int.Parse(carDetails[7 + j]);
+                    double pressure = double.Parse(carDetails[8 + j]);
+                    tires[j] = new Tire(year, pressure);
+                }
+
+                Car car = new Car(make, model, yearOfProduction, fuelQuantity, fuelConsumption, engine, tires);
+                cars.Add(car);
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}\nFuel: {car.FuelQuantity:F2}\nFuel Consumption: {car.FuelConsumption:F2}\nEngine: {car.Engine.HorsePower} horsepower, {car.Engine.CubicCapacity:F2} cubic capacity");
+                Console.WriteLine("Tires:");
+                foreach (Tire tire in car.Tires)
+                {
+                    Console.WriteLine($"Years of use: {tire.Year}");
+                    Console.WriteLine($"Pressure: {tire.Pressure}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
